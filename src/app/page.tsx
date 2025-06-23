966645646bc6d15879d0bc9bf1e4654ec2a4bc77
LeVanQuy <levanquy15052001@gmail.com>
2025-06-23 20:59:45 +0700

    use SWR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,28 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
 import app from '@/styles/app.module.css';
-import quy from '@/styles/quy.module.css'; 
+import quy from '@/styles/quy.module.css';
 import { Container } from 'react-bootstrap';
 import { useEffect } from 'react';
+import useSWR from 'swr';
 
 export default function Home() {
-  
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:8000/blogs');
-      const data = await res.json();
-      console.log(data);
-    }
-    fetchData();
-  }, []);
+
+  const fetcher = (url: string) => fetch(url).then(res => res.json());
+
+  const { data, error, isLoading } = useSWR('http://localhost:8000/blogs',
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    });
+
+  console.log('data', data);
 
   return (
     <>
-    <Container>
+      <Container>
         <ul>
           <li className={app['red']}>
             <Link href="/facebook">Facebook</Link>
