77c9f47706e5797295feefeb6dc2f8301c3e65ba
QuyLV <quylV@splus-software.com.vn>
2025-06-23 10:19:21 +0700

    using css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,19 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
+import app from '@/styles/app.module.css';
+import quy from '@/styles/quy.module.css'; 
+
 
 export default function Home() {
   return (
     <>
       <ul>
-        <li>
+        <li className={app['red']}>
           <Link href="/facebook">Facebook</Link>
         </li>
         <li>
-          <Link href="/youtube">Youtube</Link>
+          <Link href="/youtube"> <span className={quy['red']}> Youtube </span></Link>
         </li>
         <li>
           <Link href="/tiktok">Tiktok</Link>
